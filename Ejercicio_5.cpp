445c5a999bcce0c967901f2348ea84287e790459
mllangon <mllangon@myuax.com>
2023-11-15 18:10:00 +0100

    5 2

--- Ejercicio_5.cpp
+++ Ejercicio_5.cpp
@@ -11,3 +11,16 @@ int main() {
     vector.push_back(10);
     vector.push_back(20);
     vector.push_back(30);
+
+    cout << "Elementos del vector: " << endl;
+    for(int elemento : vector) {
+        cout << elemento << endl;
+    }
+    cout << endl;
+
+    std::map<int, string> mapa;
+
+    mapa[1] = "uno";
+    mapa[2] = "dos";
+    mapa[3] = "tres";
+
