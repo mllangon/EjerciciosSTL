db27ca57fac6080e1f7bb7f0e7bb273519e6b1b5
mllangon <mllangon@myuax.com>
2023-11-15 16:59:11 +0100

    1 cuadrado y sqrt

--- Ejercicio_1.cpp
+++ Ejercicio_1.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
 #include <cmath>
 
-namespace Matematicas{
+namespace Matematicas {
 
-    double suma(int a, int b){
+    double suma(int a, int b) {
         return a + b;
     }
 
-    double resta(int a, int b){
+    double resta(int a, int b) {
         return a - b;
     }
 
-    double multiplicacion(int a, int b){
+    double multiplicacion(int a, int b) {
         return a * b;
     }
 
-    double division(int a, int b){
+    double division(int a, int b) {
         if (b == 0) {
             std::cerr << "Error: División por cero." << std::endl;
             return 0;
         }
         return a / b;
+    }
+
+    double cuadrado(int a) {
+        return a * a;
+    }
+
+    double raiz(int a) {
+        if (a < 0) {
+            std::cerr << "Error: Raíz cuadrada de un número negativo." << std::endl;
+            return -1;
+        }
+        return sqrt(a);
     }
 }
