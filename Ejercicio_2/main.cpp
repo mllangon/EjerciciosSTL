5b47ce6f19bd3d03da6e6411d8938f1b19479bd8
mllangon <mllangon@myuax.com>
2023-11-15 17:19:06 +0100

    2 main

--- Ejercicio_2/main.cpp
+++ Ejercicio_2/main.cpp
@@ -1,3 +1,16 @@
-//
-// Created by llans on 15/11/2023.
-//
+#include<iostream>
+#include "triangulo.cpp"
+#include "pentagono.cpp"
+#include "circulo.cpp"
+#include "cuadrado.cpp"
+#include "rectangulo.cpp"
+using namespace std;
+
+int main(){
+    cout<<"Area del triangulo: "<<Geometria::calcularAreaTriangulo(5,3)<<endl;
+    cout<<"Area del pentagono: "<<Geometria::calcularAreaPentagono(5,3)<<endl;
+    cout<<"Area del circulo: "<<Geometria::calcularAreaCirculo(5)<<endl;
+    cout<<"Area del cuadrado: "<<Geometria::calcularAreaCuadrado(5)<<endl;
+    cout<<"Area del rectangulo: "<<Geometria::calcularAreaRectangulo(5,3)<<endl;
+    return 0;
+}
