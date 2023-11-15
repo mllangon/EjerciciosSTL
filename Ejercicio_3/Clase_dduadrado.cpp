0d94f62f09cad4e9875dc38c0b4fddb8e0764cb4
mllangon <mllangon@myuax.com>
2023-11-15 17:38:40 +0100

    3 cuadrado

---
+++ Ejercicio_3/Clase_dduadrado.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+namespace Geometria {
+    class Cuadrado {
+    private:
+        double lado;
+
+    public:
+        Cuadrado(double l) : lado(l) {}
+
+        double calcularArea() {
+            return lado * lado;
+        }
+
+        double calcularPerimetro() {
+            return 4 * lado;
+        }
+    };
--- Ejercicio_3/main.cpp
+++ Ejercicio_3/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include "Clase_circulo.cpp"
+#include "Clase_dduadrado.cpp"
+
 
 int main() {
     Geometria::Circulo circulo(5);
+    Geometria::Cuadrado cuadrado(5);
+
     std::cout << "Area del circulo: " << circulo.calcularArea() << std::endl;
     std::cout << "Perimetro del circulo: " << circulo.calcularPerimetro() << std::endl;
+
+    std::cout << "Area del cuadrado: " << cuadrado.calcularArea() << std::endl;
+    std::cout << "Perimetro del cuadrado: " << cuadrado.calcularPerimetro() << std::endl;
     return 0;
 }
