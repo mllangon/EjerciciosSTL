b46d211d6cf0cdc29e2198a8b8ee3409f3861dc0
mllangon <mllangon@myuax.com>
2023-11-15 18:10:46 +0100

    5 ultimo

--- Ejercicio_5.cpp
+++ Ejercicio_5.cpp
@@ -6,14 +6,14 @@
 using namespace std;
 
 int main() {
-    vector <int> vector;
+    vector<int> vector;
 
     vector.push_back(10);
     vector.push_back(20);
     vector.push_back(30);
 
     cout << "Elementos del vector: " << endl;
-    for(int elemento : vector) {
+    for (int elemento: vector) {
         cout << elemento << endl;
     }
     cout << endl;
@@ -23,4 +23,24 @@ int main() {
     mapa[1] = "uno";
     mapa[2] = "dos";
     mapa[3] = "tres";
+
+    cout << "Elementos del mapa: " << endl;
+    for (const auto &par: mapa) {
+        cout << par.first << " -> " << par.second << endl;
+    }
+
+    set<int> Set;
+
+    Set.insert(10);
+    Set.insert(20);
+    Set.insert(30);
+
+    cout << "Elementos del set: " << endl;
+    for (int elemento: Set) {
+        cout << elemento << " ";
+    }
+    cout << endl;
+    return 0;
+}
+
 
