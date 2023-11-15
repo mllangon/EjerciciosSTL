18882cf75ee5fa5d5060f454f7a3a62801402343
mllangon <mllangon@myuax.com>
2023-11-15 18:09:33 +0100

    5 1

---
+++ Ejercicio_5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+
+using namespace std;
+
+int main() {
+    vector <int> vector;
+
+    vector.push_back(10);
+    vector.push_back(20);
+    vector.push_back(30);
